351a458fa414a74ea5d1adea9fb3f23f3833ba5c
EAZYBLACK <68810731+EAZYBLACK@users.noreply.github.com>
2024-05-08 17:02:39 +0300

    Update OTA.sh

--- OTA.sh
+++ OTA.sh
@@ -4,21 +4,25 @@ maintainer="EAZY BLACK"                                                        #
 path=.     # Here you will need to specify the path to the crDroid source code folder
 echo Enter Device Codename
 read device
+echo Enter OEM
+read oem
+echo Enter Device Name
+read devicename
 time=$(cat $path/out/build_date.txt)                                                        # Here we get the build time
-zip=$(basename $path/out/target/product/$device/crDroidAndroid-11.0-*-$device-*.zip)        # Here we get the package name with the extension .zip
-nozip=$(basename $path/out/target/product/$device/crDroidAndroid-11.0-*-$device-*.zip .zip) # Here we get the package name without the extension .zip
+zip=$(basename $path/out/target/product/$device/lineage-16.0-*-UNOFFICIAL-$device.zip)        # Here we get the package name with the extension .zip
+nozip=$(basename $path/out/target/product/$device/lineage-16.0-*-UNOFFICIAL-$device.zip .zip) # Here we get the package name without the extension .zip
 date=$(echo $zip | cut -f3 -d '-')                                                          # Here we get the build date (in YYYY-MM-DD format)
 
 
 buildtype="Monthly"                          # choose from Testing/Alpha/Beta/Weekly/Monthly
-forum="https://t.me/emhub"   # https link (mandatory)
-gapps="https://sourceforge.net/projects/nikgapps/files/Releases/NikGapps-R/07-Mar-2024/NikGapps-core-arm64-11-20240307-signed.zip/download" #https link (leave empty if unused)
+forum="https://t.me/Redmi3IDO"   # https link (mandatory)
+gapps="https://sourceforge.net/projects/flamegapps/files/arm64/android-9/2021-08-01/FlameGApps-9.0-basic-arm64-20210801.zip/download" #https link (leave empty if unused)
 firmware=""                                  # https link (leave empty if unused)
 modem=""                                     # https link (leave empty if unused)
 bootloader=""                                # https link (leave empty if unused)
 recovery=""                                  # https link (leave empty if unused)
 paypal=""            # https link (leave empty if unused)
-telegram="https://t.me/emhub" # https link (leave empty if unused)
+telegram="https://t.me/eazy_black" # https link (leave empty if unused)
 
 #don't modify from here
 zip_name=$path/out/target/product/$device/$zip
@@ -41,7 +45,7 @@ echo '{
         "oem": "'$oem'",
         "device": "'$devicename'",
         "filename": "'$zip_only'",
-        "download": "https://github.com/WolfAURman/crdroid_ota_update/releases/download/'$nozip'/'$zip'",
+        "download": "https://github.com/EAZYBLACK/romOTA/releases/download/'$nozip'/'$zip'",
         "timestamp": '$timestamp',
         "md5": "'$md5'",
         "size": '$size',
